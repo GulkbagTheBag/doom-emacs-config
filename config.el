6d09782e394dbb9956cfaccec5183b6a14524c34
GulkbagTheBag <gulkbag3@protonmail.com>
2019-10-09 19:53:25 +0200

    Cleaning stuff and new packages

--- config.el
+++ config.el
@@ -7,8 +7,18 @@
       projectile-project-search-path '("~/dev/")
       haskell-process-type 'cabal-new-repl)
 
+(after! org
+  (setq org-directory "~/Notes/"
+        org-bullets-bullet-list '("")
+        org-agenda-files (list "~/Notes/Todo.org")))
+
 (def-package! hindent
   :config
   (add-hook! haskell-mode
              #'hindent-mode)
   )
+
+(map! (:when (featurep! :lang rust)
+        :after cargo-minor-mode
+        :map cargo-minor-mode-map
+        "SPC-b-r" rustic-cargo-run))
--- init.el
+++ init.el
@@ -2,69 +2,70 @@
 
 (doom! :input
        :completion
-       company           ; the ultimate code completion backend
-       ivy               ; a search engine for love and life
+       company
+       ivy
 
        :ui
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink the current line after jumping
-       neotree           ; a project drawer, like NERDTree for vim
-       ophints           ; highlight the region an operation acts on
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
-       treemacs          ; a project drawer, like neotree but cooler
-       vc-gutter         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       doom
+       doom-dashboard
+       doom-quit
+       hl-todo
+       modeline
+       nav-flash
+       neotree
+       ophints
+       (popup
+        +all
+        +defaults)
+       treemacs
+       vc-gutter
+       vi-tilde-fringe
+       window-select
+       workspaces
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
-       multiple-cursors  ; editing in many places at once
-       rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       (evil +everywhere)
+       file-templates
+       fold
+       multiple-cursors
+       rotate-text
+       snippets
 
        :emacs
-       dired             ; making dired pretty [functional]
-       electric          ; smarter, keyword-based electric-indent
-       vc                ; version-control and Emacs, sitting in a tree
+       dired
+       electric
+       vc
 
        :term
-       eshell            ; a consistent, cross-platform shell (WIP)
+       eshell
 
        :tools
-       eval              ; run code, run (also, repls)
-       flycheck          ; tasing you for every semicolon you forget
-       (lookup           ; helps you navigate your code and documentation
-        +docsets)        ; ...or in Dash docsets locally
-       magit             ; a git porcelain for Emacs
+       eval
+       flycheck
+       (lookup
+        +docsets)
+       magit
+       lsp
 
        :lang
-       assembly          ; assembly for fun or debugging
-       cc                ; C/C++/Obj-C madness
-       data              ; config/data formats
-       emacs-lisp        ; drown in parentheses
-       haskell           ; a language that's lazier than I am
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
-       nim               ; python + lisp at the speed of c
-       nix               ; I hereby declare "nix geht mehr!"
-       (org              ; organize your plain life in plain text
-        +dragndrop       ; drag & drop files/images into org buffers
-        +ipython         ; ipython/jupyter support for babel
-        +pandoc          ; export-with-pandoc support
-        +present)        ; using org-mode for presentations
-       python            ; beautiful is better than ugly
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       assembly
+       cc
+       data
+       emacs-lisp
+       haskell
+       javascript
+       lua
+       markdown
+       nim
+       nix
+       (org
+        +dragndrop
+        +ipython
+        +pandoc
+        +present)
+       python
+       rust
+       sh
 
        :email
        :app
